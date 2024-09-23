8a80835f3ffef405dd27d8cc38a1b6de0fba0fae
emgixiii <emgi@live.in>
2024-09-24 03:00:45 +0530

    FEAT find delaunay triangles

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from scipy import ndimage
+from scipy.spatial import Delaunay
 from PIL import Image
 
 
@@ -38,31 +39,51 @@ def getBlurRadius(img: np.ndarray):
 
 def main():
     # Get all files
-    for vid_no in range(1, 7):
+    for vid_no in range(2, 7):
         files = glob(f"./raw/frames/src_{vid_no}_*.png")
         files.sort()
         rad = 0
         for file in files:
+            if rad != 0:
+                return
             frame_no = int(file.split("_")[-1].split(".")[0])
             img = loadImage(file, True)
             ori_img = loadImage(file)
+            # Get optimal blur radius
             if rad == 0:
                 rad = getBlurRadius(img)
                 print(f"Set radius to {rad:5.2f}px")
+            # Find all centers of shadows (atoms)
             bl_img = blurImg(img, rad)
             min_img = ndimage.minimum_filter(bl_img, rad * 2)
             yInd, xInd = np.where(min_img == bl_img)
             print(f"Found {len(yInd):6d} centers")
-            plt.imshow(ori_img / 255.0 / 3.0)
-            plt.scatter(xInd, yInd, s=0.2, c="cyan")
+            # plt.imshow(ori_img / 255.0 / 3.0)
+            # plt.scatter(xInd, yInd, s=0.2, c="cyan")
+            # plt.axis("off")
+            # plt.savefig(
+            #     f"./raw/centers/{vid_no}_{frame_no}.png",
+            #     bbox_inches="tight",
+            #     pad_inches=0,
+            #     dpi=300,
+            # )
+            # plt.close()
+            delaunay = Delaunay(np.column_stack(np.where(min_img == bl_img)))
+            plt.imshow(ori_img / 255.0 / 4.0)
+            for tri in delaunay.simplices:
+                tri_pos = delaunay.points[np.concatenate((tri, tri[:1]))]
+                plt.plot(tri_pos[:, 1], tri_pos[:, 0], linewidth=0.1, c="white")
             plt.axis("off")
             plt.savefig(
-                f"./raw/centers/{vid_no}_{frame_no}.png",
+                f"./raw/delaunay/{vid_no}_{frame_no}.png",
                 bbox_inches="tight",
                 pad_inches=0,
                 dpi=300,
             )
             plt.close()
+            nb = []
+            for i in range(len(delaunay.points)):
+                nb.append(np.sum(delaunay.simplices == i))
 
 
 if __name__ == "__main__":
