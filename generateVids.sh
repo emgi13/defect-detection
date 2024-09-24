8e455ee760dc0506beb62e420c1c4a378ae25c2f
emgixiii <emgi@live.in>
2024-09-24 13:00:35 +0530

    FIX fix all image sizing and ADD voronoi interpolation

--- generateVids.sh
+++ generateVids.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+framerate=8
+
 # Generate centers video
-# for vid_no in {1..6}; do
-#     ffmpeg -framerate 24 -i ./raw/centers/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/centers_${vid_no}.mp4 -y
-# done
+for vid_no in {1..6}; do
+    ffmpeg -framerate $framerate -i ./raw/centers/${vid_no}_%d.png -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/centers_${vid_no}.mp4 -y
+done
 
 # Generate delaunay video
 for vid_no in {1..6}; do
-    ffmpeg -framerate 24 -i ./raw/delaunay/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/delaunay_${vid_no}.mp4 -y
+    ffmpeg -framerate $framerate -i ./raw/delaunay/${vid_no}_%d.png -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/delaunay_${vid_no}.mp4 -y
 done
 
 # Generate bond_order video
 for vid_no in {1..6}; do
-    ffmpeg -framerate 24 -i ./raw/bond_order/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/bond_order_${vid_no}.mp4 -y
+    ffmpeg -framerate $framerate -i ./raw/bond_order/${vid_no}_%d.png -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/bond_order_${vid_no}.mp4 -y
 done
+
+# Generate voronoi video
+for vid_no in {1..6}; do
+    ffmpeg -framerate $framerate -i ./raw/voronoi/${vid_no}_%d.png -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/voronoi_${vid_no}.mp4 -y
+done
+
+# Generate voronoi_interp video
+for vid_no in {1..6}; do
+    ffmpeg -framerate $framerate -i ./raw/voronoi_interp/${vid_no}_%d.png -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/voronoi_interp_${vid_no}.mp4 -y
+done
+
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from scipy import ndimage
-from scipy.spatial import Delaunay, Voronoi
+from scipy.spatial import Delaunay, Voronoi, ConvexHull
 from scipy.interpolate import griddata
 from PIL import Image
+
+
+def reject_outliers(data, m=2.0):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d / mdev if mdev else np.zeros(len(d))
+    return data[s < m]
 
 
 def loadImage(path: str, gray=False) -> np.ndarray:
@@ -40,10 +47,12 @@ def getBlurRadius(img: np.ndarray):
 
 def main():
     # Get all files
-    for vid_no in range(2, 7):
+    for vid_no in range(1, 7):
         files = glob(f"./raw/frames/src_{vid_no}_*.png")
         files.sort()
         rad = 0
+        clip_min = 0
+        clip_max = 0
         for file in files:
             # if rad != 0:
             #     return
@@ -59,57 +68,121 @@ def main():
             min_img = ndimage.minimum_filter(bl_img, rad * 2)
             yInd, xInd = np.where(min_img == bl_img)
             print(f"Found {len(yInd):6d} centers")
-            # plt.imshow(ori_img / 255.0 / 3.0)
-            # plt.scatter(xInd, yInd, s=0.2, c="cyan")
-            # plt.axis("off")
-            # plt.savefig(
-            #     f"./raw/centers/{vid_no}_{frame_no}.png",
-            #     bbox_inches="tight",
-            #     pad_inches=0,
-            #     dpi=300,
-            # )
-            # plt.close()
-            # delaunay = Delaunay(np.column_stack(np.where(min_img == bl_img)))
-            # plt.imshow(ori_img / 255.0 / 4.0)
-            # for tri in delaunay.simplices:
-            #     tri_pos = delaunay.points[np.concatenate((tri, tri[:1]))]
-            #     plt.plot(tri_pos[:, 1], tri_pos[:, 0], linewidth=0.1, c="white")
-            # plt.axis("off")
-            # plt.savefig(
-            #     f"./raw/delaunay/{vid_no}_{frame_no}.png",
-            #     bbox_inches="tight",
-            #     pad_inches=0,
-            #     dpi=300,
-            # )
-            # plt.close()
-            # nb = []
-            # for i in range(len(delaunay.points)):
-            #     nb.append(np.sum(delaunay.simplices == i))
-            # grid_x, grid_y = np.mgrid[0 : img.shape[0] : 1, 0 : img.shape[1] : 1]
-            # grid_z = griddata(
-            #     delaunay.points, nb, (grid_x, grid_y), method="linear", fill_value=0
-            # )
-            # plt.imshow(grid_z, cmap="plasma", vmin=0.0, vmax=7.0, interpolation="none")
-            # plt.axis("off")
-            # plt.savefig(
-            #     f"./raw/bond_order/{vid_no}_{frame_no}.png",
-            #     bbox_inches="tight",
-            #     pad_inches=0,
-            #     dpi=300,
-            # )
-            # plt.close()
-            vor = Voronoi(np.column_stack(np.where(min_img == bl_img)))
+
+            plt.figure(figsize=(img.shape[1] / 300, img.shape[0] / 300))
+            plt.imshow(ori_img / 255.0 / 3.0)
+            plt.scatter(xInd, yInd, s=0.2, c="cyan")
+            plt.axis("off")
+            plt.ylim((0, img.shape[0] - 1))
+            plt.xlim((0, img.shape[1] - 1))
+            plt.savefig(
+                f"./raw/centers/{vid_no}_{frame_no}.png",
+                bbox_inches="tight",
+                pad_inches=0,
+                dpi=300,
+            )
+            plt.close()
+            delaunay = Delaunay(np.column_stack(np.where(min_img == bl_img)))
+            plt.figure(figsize=(img.shape[1] / 300, img.shape[0] / 300))
             plt.imshow(ori_img / 255.0 / 4.0)
-            for poly in vor.regions:
+            for tri in delaunay.simplices:
+                tri_pos = delaunay.points[np.concatenate((tri, tri[:1]))]
+                plt.plot(tri_pos[:, 1], tri_pos[:, 0], linewidth=0.1, c="white")
+            plt.axis("off")
+            plt.ylim((0, img.shape[0] - 1))
+            plt.xlim((0, img.shape[1] - 1))
+            plt.savefig(
+                f"./raw/delaunay/{vid_no}_{frame_no}.png",
+                bbox_inches="tight",
+                pad_inches=0,
+                dpi=300,
+            )
+            plt.close()
+            nb = []
+            for i in range(len(delaunay.points)):
+                nb.append(np.sum(delaunay.simplices == i))
+            grid_x, grid_y = np.mgrid[0 : img.shape[0] : 1, 0 : img.shape[1] : 1]
+            grid_z = griddata(
+                delaunay.points, nb, (grid_x, grid_y), method="linear", fill_value=0
+            )
+            plt.figure(figsize=(img.shape[1] / 300, img.shape[0] / 300))
+            plt.imshow(grid_z, cmap="plasma", vmin=0.0, vmax=7.0, interpolation="none")
+            plt.axis("off")
+            plt.savefig(
+                f"./raw/bond_order/{vid_no}_{frame_no}.png",
+                bbox_inches="tight",
+                pad_inches=0,
+                dpi=300,
+            )
+            plt.close()
+            vor = Voronoi(np.column_stack(np.where(min_img == bl_img)))
+            plasma = plt.get_cmap("plasma")
+
+            # generate clipping values
+            if clip_min == 0 and clip_max == 0:
+                areas = []
+                for i in range(len(vor.points)):
+                    poly = vor.regions[vor.point_region[i]]
+                    if len(poly) == 0 or -1 in poly:
+                        continue
+                    poly_pos = vor.vertices[poly]
+                    area = ConvexHull(poly_pos).area / rad / rad
+                    areas.append(area)
+                areas = reject_outliers(np.array(areas))
+                avg = np.average(areas)
+                std = np.std(areas)
+                clip_min = avg - 7 * std
+                clip_max = avg + 7 * std
+                print(f"Setting clipping to {clip_min:5.3f}:{clip_max:5.2f}")
+
+            def get_color(x):
+                return plasma(np.clip((x - clip_min) / (clip_max - clip_min), 0, 1))
+
+            areas = []
+            verts = []
+            plt.figure(figsize=(img.shape[1] / 300, img.shape[0] / 300))
+            for i in range(len(vor.points)):
+                poly = vor.regions[vor.point_region[i]]
                 if len(poly) == 0 or -1 in poly:
                     continue
-                poly_pos = vor.vertices[np.concatenate((poly, poly[:1]))]
-                plt.plot(poly_pos[:, 1], poly_pos[:, 0], linewidth=0.1, c="white")
+                poly_pos = vor.vertices[poly]
+                area = ConvexHull(poly_pos).area / rad / rad
+                areas.append(area)
+                verts.append(vor.points[i])
+                plt.fill(
+                    poly_pos[:, 1],
+                    poly_pos[:, 0],
+                    facecolor=get_color(area),
+                    edgecolor=get_color(area),
+                    linewidth=0.5,
+                )
             plt.ylim((0, img.shape[0] - 1))
             plt.xlim((0, img.shape[1] - 1))
             plt.axis("off")
             plt.savefig(
                 f"./raw/voronoi/{vid_no}_{frame_no}.png",
+                bbox_inches="tight",
+                pad_inches=0,
+                dpi=300,
+            )
+            plt.close()
+            verts = np.array(verts)
+            areas = np.array(areas)
+            colors = get_color(areas)
+            grid_x, grid_y = np.mgrid[0 : img.shape[0] : 1, 0 : img.shape[1] : 1]
+            grid_z = griddata(
+                verts, colors, (grid_x, grid_y), method="cubic", fill_value=0
+            )
+            plt.figure(figsize=(img.shape[1] / 300, img.shape[0] / 300))
+            plt.imshow(
+                grid_z,
+                interpolation="none",
+            )
+            plt.axis("off")
+            plt.ylim((0, img.shape[0] - 1))
+            plt.xlim((0, img.shape[1] - 1))
+            plt.savefig(
+                f"./raw/voronoi_interp/{vid_no}_{frame_no}.png",
                 bbox_inches="tight",
                 pad_inches=0,
                 dpi=300,
