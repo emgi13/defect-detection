733d290643aab3a3d53c13b0af185125127c1d56
emgixiii <emgi@live.in>
2024-09-24 10:16:32 +0530

    FEAT calculate voronoi regions

--- generateVids.sh
+++ generateVids.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Generate centers video
+# for vid_no in {1..6}; do
+#     ffmpeg -framerate 24 -i ./raw/centers/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/centers_${vid_no}.mp4 -y
+# done
+
+# Generate delaunay video
 for vid_no in {1..6}; do
-    ffmpeg -framerate 24 -i ./raw/centers/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/centers_${vid_no}.mp4 -y
+    ffmpeg -framerate 24 -i ./raw/delaunay/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/delaunay_${vid_no}.mp4 -y
 done
 
+# Generate bond_order video
+for vid_no in {1..6}; do
+    ffmpeg -framerate 24 -i ./raw/bond_order/${vid_no}_%d.png -vf "scale=iw:ih-mod(ih\,2)" -c:v libx264 -pix_fmt yuv420p ./raw/bond_order_${vid_no}.mp4 -y
+done
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from scipy import ndimage
-from scipy.spatial import Delaunay
+from scipy.spatial import Delaunay, Voronoi
 from scipy.interpolate import griddata
 from PIL import Image
 
@@ -40,7 +40,7 @@ def getBlurRadius(img: np.ndarray):
 
 def main():
     # Get all files
-    for vid_no in range(1, 7):
+    for vid_no in range(2, 7):
         files = glob(f"./raw/frames/src_{vid_no}_*.png")
         files.sort()
         rad = 0
@@ -69,30 +69,47 @@ def main():
             #     dpi=300,
             # )
             # plt.close()
-            delaunay = Delaunay(np.column_stack(np.where(min_img == bl_img)))
+            # delaunay = Delaunay(np.column_stack(np.where(min_img == bl_img)))
+            # plt.imshow(ori_img / 255.0 / 4.0)
+            # for tri in delaunay.simplices:
+            #     tri_pos = delaunay.points[np.concatenate((tri, tri[:1]))]
+            #     plt.plot(tri_pos[:, 1], tri_pos[:, 0], linewidth=0.1, c="white")
+            # plt.axis("off")
+            # plt.savefig(
+            #     f"./raw/delaunay/{vid_no}_{frame_no}.png",
+            #     bbox_inches="tight",
+            #     pad_inches=0,
+            #     dpi=300,
+            # )
+            # plt.close()
+            # nb = []
+            # for i in range(len(delaunay.points)):
+            #     nb.append(np.sum(delaunay.simplices == i))
+            # grid_x, grid_y = np.mgrid[0 : img.shape[0] : 1, 0 : img.shape[1] : 1]
+            # grid_z = griddata(
+            #     delaunay.points, nb, (grid_x, grid_y), method="linear", fill_value=0
+            # )
+            # plt.imshow(grid_z, cmap="plasma", vmin=0.0, vmax=7.0, interpolation="none")
+            # plt.axis("off")
+            # plt.savefig(
+            #     f"./raw/bond_order/{vid_no}_{frame_no}.png",
+            #     bbox_inches="tight",
+            #     pad_inches=0,
+            #     dpi=300,
+            # )
+            # plt.close()
+            vor = Voronoi(np.column_stack(np.where(min_img == bl_img)))
             plt.imshow(ori_img / 255.0 / 4.0)
-            for tri in delaunay.simplices:
-                tri_pos = delaunay.points[np.concatenate((tri, tri[:1]))]
-                plt.plot(tri_pos[:, 1], tri_pos[:, 0], linewidth=0.1, c="white")
+            for poly in vor.regions:
+                if len(poly) == 0 or -1 in poly:
+                    continue
+                poly_pos = vor.vertices[np.concatenate((poly, poly[:1]))]
+                plt.plot(poly_pos[:, 1], poly_pos[:, 0], linewidth=0.1, c="white")
+            plt.ylim((0, img.shape[0] - 1))
+            plt.xlim((0, img.shape[1] - 1))
             plt.axis("off")
             plt.savefig(
-                f"./raw/delaunay/{vid_no}_{frame_no}.png",
-                bbox_inches="tight",
-                pad_inches=0,
-                dpi=300,
-            )
-            plt.close()
-            nb = []
-            for i in range(len(delaunay.points)):
-                nb.append(np.sum(delaunay.simplices == i))
-            grid_x, grid_y = np.mgrid[0 : img.shape[0] : 1, 0 : img.shape[1] : 1]
-            grid_z = griddata(
-                delaunay.points, nb, (grid_x, grid_y), method="cubic", fill_value=0
-            )
-            plt.imshow(grid_z, cmap="inferno")
-            plt.axis("off")
-            plt.savefig(
-                f"./raw/bond_order/{vid_no}_{frame_no}.png",
+                f"./raw/voronoi/{vid_no}_{frame_no}.png",
                 bbox_inches="tight",
                 pad_inches=0,
                 dpi=300,
